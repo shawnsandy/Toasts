87118c2fcffc913280b302c1a7e10fe0b0370556
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-10-30 19:00:27 -0400

    adds npm script to copy toasts files

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by shawnsandy on 10/30/16.
+ */
+gulp = require ('gulp');
+
+gulp.task('copy:js', function () {
+    gulp.src([
+        './node_modules/toast.js/toast.css',
+        './node_modules/toast.js/toast.js',
+        './node_modules/toastr/build/toastr.min.css',
+        './node_modules/toastr/build/toastr.min.js'
+    ])
+        .pipe(gulp.dest('./src/public/js/toasts'));
+});
