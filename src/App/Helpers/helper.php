58101f10d96c0cbe9b5c8a3d84feadb4ee86e8bb
shawn sandy <shawnsandy04@gmail.com>
2016-11-03 09:13:33 -0400

    PHCS/PCHBD fixes

--- src/App/Helpers/helper.php
+++ src/App/Helpers/helper.php
@@ -1,11 +1,13 @@
 <?php
 
 
-if (!function_exists('createToast')):
+if (!function_exists('createToast')) :
+
 
     /**
-     * @param string $message
-     * @param string|null $title
+     * @param string      $message
+     * @param null|string $title
+     * @param string      $type
      */
     function createToast($message = 'Info', $title = null, $type = 'info')
     {
@@ -15,26 +17,26 @@ if (!function_exists('createToast')):
     }
 
 
-    if (!function_exists('toastInfo')):
+    if (!function_exists('toastInfo')) :
 
         /**
-         * @param string $message
+         * @param string      $message
          * @param string|null $title
          */
         function toastInfo($message = 'Info', $title = null)
         {
 
-            createToast($message, $title, 'info');
+            createToast($message, $title);
 
         }
 
     endif;
 
 
-    if (!function_exists('toastSuccess')):
+    if (!function_exists('toastSuccess')) :
 
         /**
-         * @param string $message
+         * @param string      $message
          * @param string|null $title
          */
         function toastSuccess($message = 'Success!', $title = null)
@@ -47,10 +49,10 @@ if (!function_exists('createToast')):
     endif;
 
 
-    if (!function_exists('toastError')):
+    if (!function_exists('toastError')) :
 
         /**
-         * @param string $message
+         * @param string      $message
          * @param string|null $title
          */
         function toastError($message = 'Error!', $title = null)
@@ -63,10 +65,10 @@ if (!function_exists('createToast')):
     endif;
 
 
-    if (!function_exists('toastWarning')):
+    if (!function_exists('toastWarning')) :
 
         /**
-         * @param string $message
+         * @param string      $message
          * @param string|null $title
          */
         function toastWarning($message = 'Warning!', $title = null)
