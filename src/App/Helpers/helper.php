a667fffbe7543cb93c6d1e0e195760d56fdd9ab0
shawn sandy <shawnsandy04@gmail.com>
2016-11-03 09:08:04 -0400

    refactors functions in helpers.php
    - adds createtoast()
    - generates flash inside functions

--- src/App/Helpers/helper.php
+++ src/App/Helpers/helper.php
@@ -1,65 +1,81 @@
 <?php
 
 
-if (!function_exists('toastInfo')):
+if (!function_exists('createToast')):
 
     /**
      * @param string $message
      * @param string|null $title
      */
-    function toastInfo($message = 'Info', $title = null )
+    function createToast($message = 'Info', $title = null, $type = 'info')
     {
         $app = app('Toast');
-        $app->flash($message, 'info', $title);
+        $app->flash($message, $type, $title);
 
     }
 
-endif;
+
+    if (!function_exists('toastInfo')):
+
+        /**
+         * @param string $message
+         * @param string|null $title
+         */
+        function toastInfo($message = 'Info', $title = null)
+        {
+
+            createToast($message, $title, 'info');
+
+        }
+
+    endif;
 
 
-if (!function_exists('toastSuccess')):
+    if (!function_exists('toastSuccess')):
 
-    /**
-     * @param string $message
-     * @param string|null $title
-     */
-    function toastSuccess($message = 'Success!', $title = null )
-    {
-        $app = app('Toast');
-        $app->flash($message, 'success', $title);
+        /**
+         * @param string $message
+         * @param string|null $title
+         */
+        function toastSuccess($message = 'Success!', $title = null)
+        {
 
-    }
+            createToast($message, $title, 'success');
 
-endif;
+        }
 
-
-if (!function_exists('toastError')):
-
-    /**
-     * @param string $message
-     * @param string|null $title
-     */
-    function toastError($message = 'Error!', $title = null )
-    {
-        $app = app('Toast');
-        $app->flash($message, 'error', $title);
-
-    }
-
-endif;
+    endif;
 
 
-if (!function_exists('toastWarning')):
+    if (!function_exists('toastError')):
 
-    /**
-     * @param string $message
-     * @param string|null $title
-     */
-    function toastWarning($message = 'Warning!', $title = null )
-    {
-        $app = app('Toast');
-        $app->flash($message, 'warning', $title);
+        /**
+         * @param string $message
+         * @param string|null $title
+         */
+        function toastError($message = 'Error!', $title = null)
+        {
 
-    }
+            createToast($message, $title, 'error');
+
+        }
+
+    endif;
+
+
+    if (!function_exists('toastWarning')):
+
+        /**
+         * @param string $message
+         * @param string|null $title
+         */
+        function toastWarning($message = 'Warning!', $title = null)
+        {
+
+            createToast($message, $title, 'warning');
+
+        }
+
+    endif;
 
 endif;
